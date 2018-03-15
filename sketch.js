c9e4f2f12265724816448709ffe356975952bfb9
AuditRahman <36421517+AuditRahman@users.noreply.github.com>
2018-03-14 21:08:02 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   noCanvas();
 
   let bot = new RiveScript();
-  bot.loadFile("Med1.rive", brainReady, brainError);
+  bot.loadFile("brain.rive", brainReady, brainError);
 
   function brainReady() {
     console.log('Chatbot ready!');
